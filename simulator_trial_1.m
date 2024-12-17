03b45322a45a227c67843f07e04869ff662d29bb
megha <meghavenkatesam@gmail.com>
2024-12-17 15:47:52 -0500

    notworking_3link
    notworking_3link

--- simulator_trial_1.m
+++ simulator_trial_1.m
@@ -14,6 +14,12 @@ W2 = 0.13; % [m]
 H2 = 0.11; % [m]
 m2 = 0.007;  % [kg]
 
+% Link 2 dimensions
+L3 = 0.23;  % [m]
+W3 = 0.12; % [m]
+H3 = 0.11; % [m]
+m3 = 0.006;  % [kg]
+
 % Gravity
 g = -9.81; % [m/s^2]
 % g = 0; % [m/s^2]
@@ -28,6 +34,11 @@ I1 = I1_cm + m1*d1^2; % parallel axis shift for link 1
 I2_cm = (1/12)*m2*(H2^2 + W2^2);
 % For link 2, we use I2_cm directly in the standard formula for a 2-link arm
 I2 = I2_cm;
+
+% Link 3 Inertia
+I3_cm = (1/12)*m3*(H3^2 + W3^2);
+I3 = I3_cm; % Axis through CM of Link 3
+lc3 = L3/2;
 
 %% Tendon Parameters
 % From the snippet, we have four tendons: h1, h2, h3, h4.
@@ -86,24 +97,41 @@ P_theta = @(theta) dH_dtheta(theta)';
 % Define link centers of mass
 lc1 = L1/2;
 lc2 = L2/2;
+lc3 = L3/2;
 
 % Standard parameters from 2-link arm dynamics
 a_m = m1*lc1^2 + m2*(L1^2+lc2^2) + I1 + I2;
 b_m = m2*L1*lc2;
 d_m = m2*lc2^2 + I2;
 
-M_theta = @(th) [a_m+2*b_m*cos(th(2)), d_m+b_m*cos(th(2));
-                 d_m+b_m*cos(th(2)),   d_m];
+% Now add Link 3, which is rigidly attached at the end of Link 2:
+% The center of mass of Link 3 is at (L1, L2+lc3) from the first joint.
+% For the total system:
+% a_new = a + m3*(L1^2 + (L2+lc3)^2) + I3
+% b_new = b + m3*L1*(L2+lc3)
+% d_new = d + m3*(L2+lc3)^2 + I3
 
-C_theta = @(th, thd) [ -b_m*sin(th(2))*thd(2), -b_m*sin(th(2))*(thd(1)+thd(2));
-                        b_m*sin(th(2))*thd(1), 0 ];
+a_orig = m1*lc1^2 + m2*(L1^2+lc2^2) + I1 + I2;
+b_orig = m2*L1*lc2;
+d_orig = m2*lc2^2 + I2;
 
-N_theta = @(th) [-(m1*g*lc1*cos(th(1)) + m2*g*L1*cos(th(1)))+m2*g*lc2*cos(th(1)-th(2));
-                 -m2*g*lc2*cos(th(1)-th(2))];
+a_new = a_orig + m3*(L1^2 + (L2+lc3)^2) + I3;
+b_new = b_orig + m3*L1*(L2+lc3);
+d_new = d_orig + m3*(L2+lc3)^2 + I3;
+
+% Mass Matrix
+M_theta = @(th) [a_new+2*b_new*cos(th(2)), d_new+b_new*cos(th(2));
+                 d_new+b_new*cos(th(2)),   d_new];
+
+% Coriolis Matrix remains structurally the same, just with updated parameters
+C_theta = @(th, thd) [ -b_new*sin(th(2))*thd(2), -b_new*sin(th(2))*(thd(1)+thd(2));
+                        b_new*sin(th(2))*thd(1), 0 ];
+
+N_theta = @(th) [-(m1*g*lc1*cos(th(1)) + m2*g*L1*cos(th(1)))+m2*g*lc2*cos(th(1)-th(2)) + m3*g*(L1*cos(th(1))+(L2+lc3)*cos(th(1)-2*th(2)));
+                 -m2*g*lc2*cos(th(1)-th(2)) + m3*g*(L2+lc3)*cos(th(1)-2*th(2))];
 
 % Equation of motion:
 % M(theta)*ddtheta + C(theta,theta_dot)*theta_dot + N(theta) = P(theta)*f
-% => ddtheta = M(theta)\( P(theta)*f - C(theta,theta_dot)*theta_dot - N(theta) )
 
 theta_ddot_func = @(th, thd, t) M_theta(th)\(P_theta(th)*f(t) - C_theta(th, thd)*thd - N_theta(th));
 
@@ -124,30 +152,15 @@ theta_ddot = zeros(2, n);
 theta(:,1) = theta0;
 theta_dot(:,1) = theta_dot0;
 
-%% Numerical Integration (Euler's Method)
-
-% for i = 1:n-1
-%     f_current = f(t(i)); % Compute time-varying force
-%     theta_ddot(:, i) = M_theta(theta(:, i))\(P_theta(theta(:, i))*f_current ...
-%                        - C_theta(theta(:, i), theta_dot(:, i))*theta_dot(:, i) ...
-%                        - N_theta(theta(:, i)));
-%     theta_dot(:, i+1) = theta_dot(:, i) + theta_ddot(:, i)*dt;
-%     theta(:, i+1) = theta(:, i) + theta_dot(:, i)*dt;
-% end
-
-theta_ddot(:, end) = theta_ddot_func(theta(:, end), theta_dot(:, end), t(end));
-
-% Desired limits in radians
+% Angle limits (same as before)
 theta1_min = deg2rad(0);
 theta1_max = deg2rad(90);
 theta2_min = deg2rad(-90);
 theta2_max = deg2rad(0);
 
+%% Numerical Integration (Euler's Method)
 for i = 1:n-1
-    f_current = f(t(i));
-    theta_ddot(:, i) = M_theta(theta(:, i))\(P_theta(theta(:, i))*f_current ...
-                       - C_theta(theta(:, i), theta_dot(:, i))*theta_dot(:, i) ...
-                       - N_theta(theta(:, i)));
+    theta_ddot(:, i) = theta_ddot_func(theta(:, i), theta_dot(:, i), t(i));
     theta_dot(:, i+1) = theta_dot(:, i) + theta_ddot(:, i)*dt;
     theta_temp = theta(:, i) + theta_dot(:, i)*dt;
 
@@ -172,6 +185,7 @@ for i = 1:n-1
     theta(:, i+1) = theta_temp;
 end
 
+theta_ddot(:, end) = theta_ddot_func(theta(:, end), theta_dot(:, end), t(end));
 
 %% Plot Results
 figure;
@@ -199,7 +213,7 @@ legend('\theta_1 dot', '\theta_2 dot');
 grid on;
 
 %% Animation of the Manipulator
-figure('Name', '2-Link Manipulator Animation');
+figure('Name', '3-Link Manipulator Animation (Link 3 rigidly attached)');
 axis equal;
 xlim([-1, 1]);
 ylim([-1, 1]);
@@ -210,18 +224,30 @@ grid on; hold on;
 link1_plot = line([0,0],[0,0],'LineWidth',4,'Color','b');
 link2_plot = line([0,0],[0,0],'LineWidth',4,'Color','r');
 
+% To visualize Link 3, treat it as extending from the end of Link 2
+% The angle for Link 3 is the same as Link 2's absolute angle (θ1+θ2)
+% We'll draw Link 3 in green.
+link3_plot = line([0,0],[0,0],'LineWidth',4,'Color','g');
+
 for i=1:10:n
     th1 = theta(1,i);
     th2 = theta(2,i);
 
+    % Positions of joints
     x1 = L1*cos(th1);
     y1 = L1*sin(th1);
+
     x2 = x1 + L2*cos(th1+th2);
     y2 = y1 + L2*sin(th1+th2);
 
+    % Link 3 end point:
+    x3 = x2 + L3*cos(th1+th2); 
+    y3 = y2 + L3*sin(th1+th2);
+
     set(link1_plot, 'XData',[0,x1],'YData',[0,y1]);
     set(link2_plot, 'XData',[x1,x2],'YData',[y1,y2]);
+    set(link3_plot, 'XData',[x2,x3],'YData',[y2,y3]);
 
-    pause(.05);
+    pause(0.05);
     drawnow;
 end
